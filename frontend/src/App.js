4b52749f5b30260ebc46985b94e934e85d81f9bb
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-10-01 12:29:03 +0300

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import Video from "./pages/Video";
+import Storage from "./pages/Storage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/login" element={<Login/>}>
+              </Route>
+              <Route path="/chat" element={<Chat/>}>
+              </Route>
+              <Route path="/make" element={<Video/>}>
+              </Route>
+              <Route path="/storage" element={<Storage/>}>
+              </Route>
+              <Route path="/mypage" element={<MyPage/>}>
+              </Route>
+          </Routes>
+      </BrowserRouter>
+
   );
 }
 
