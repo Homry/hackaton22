1b0ba2e28e48ea1bbc2396ca42efff4318e9b0e8
Sergey Sorokumov <ssorokumov@gmail.com>
2022-10-02 05:39:26 +0300

    WIP download

--- backend/routes/get_gifs.py
+++ backend/routes/get_gifs.py
@@ -1,11 +1,6 @@
-from flask import request, send_file
-import base64
-from PIL import Image
-import io
-import numpy as np
+from flask import request, send_file, send_from_directory
 import tempfile
-from processing import convert_image as converter
-from processing import create_gif
+
 from bson.objectid import ObjectId
 from app import app, db
 import gridfs
@@ -14,17 +9,33 @@ import gridfs
 def get_gifs(token):
     dataBase = db.users_gifs.fs.files
     links = []
+    downloads = []
     for i in dataBase.find({'name': token}):
-        print(i["_id"])
         links.append(f'http://127.0.0.1:5000/get_gif/{i["_id"]}')
-    print(links)
-    return {'status': 'ok', 'links': links}, 200
+        downloads.append(f'http://127.0.0.1:5000/download/{i["_id"]}')
+    return {'status': 'ok', 'links': links, 'download': downloads}, 200
 
 @app.route('/get_gif/<token>', methods=['GET'])
 def get_gif(token):
+    print(token)
     dataBase = db.users_gifs
     fs = gridfs.GridFS(dataBase)
     gif = fs.find_one({'_id': ObjectId(token)})
     with tempfile.NamedTemporaryFile(mode="wb", suffix='.gif') as gifFile:
         gifFile.write(gif.read())
         return send_file(gifFile.name, mimetype='image/gif')
+
+@app.route('/download/<token>', methods=['GET', 'POST'])
+def download(token):
+    dataBase = db.users_gifs
+    fs = gridfs.GridFS(dataBase)
+    gif = fs.find_one({'_id': ObjectId(token)})
+    with tempfile.NamedTemporaryFile(mode="wb", suffix='.gif') as gifFile:
+        gifFile.write(gif.read())
+        print(gifFile.name)
+        names = gifFile.name.split('/')
+        filename = names[-1]
+        names.pop(-1)
+        directory = '/'.join(names)
+
+        return send_file(gifFile.name,  mimetype='image/gif')
