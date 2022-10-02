7a37f96a75be702a677c4b64aad51169e28e2ce5
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-10-02 07:32:33 +0300

    some css

--- frontend/src/pages/Storage.js
+++ frontend/src/pages/Storage.js
@@ -2,7 +2,7 @@ import VideoFromDevice from "../components/VideoFromDevice";
 import {useEffect, useState} from "react";
 
 export default function Storage(props) {
-    console.log(props.user['_id'])
+    console.log(props.user)
     const [links, setLinks] = useState({'links':[]});
     useEffect(()=>{
         fetch(`http://127.0.0.1:5000/get_gifs/${props.user['_id']}`).then(res=> res.json())
@@ -15,9 +15,13 @@ export default function Storage(props) {
         <>
             {
                 Array.from(Array(links['links'].length)).map((_, index) => (
-                    <div>
-                        <img src={links['links'][index]}/>
-                        <a href={links['download'][index]}>Скачать</a>
+                    <div >
+                        <img style={{left: "33%"}} src={links['links'][index]}/>
+                        <br/>
+                        <a style={{background: "#04AA6D", color: "white", left: "33%"}} href={links['download'][index]}>Скачать</a>
+
+                        <br/>
+
                     </div>
 
                 ))
